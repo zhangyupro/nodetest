8fe0e27b08a40a6069205c10e1ffa3d706f7c4f3
liujinpeng <6865567@qq.com>
2023-02-09 20:13:09 +0800

    test

--- controllers/work_group.ctrl.js
+++ controllers/work_group.ctrl.js
@@ -1,12 +1,17 @@
-const tableName = 'work_group'
-const commonDB = require('../db/commondb.v2')
+const tableName = "work_group";
+const commonDB = require("../db/commondb.v2");
 
 exports.setGroupName = async (array) => {
-
-    for (let i = 0; i < array.total; i++) {
-        if (array.content[i].hasOwnProperty('group_id')) {
-            console.log(array.content[i])
-            array.content[i].work_group = await commonDB.selectById(array.content[i].group_id , tableName)
-        }
+  await array.content.forEach(async (e) => {
+    if (e.hasOwnProperty("group_id")) {
+      e.work_group = await commonDB.selectById(e.group_id, tableName);
     }
-}
+  });
+
+  // for (let i = 0; i < array.total; i++) {
+  //     if (array.content[i].hasOwnProperty('group_id')) {
+  //         console.log(array.content[i])
+  //         array.content[i].work_group = await commonDB.selectById(array.content[i].group_id , tableName)
+  //     }
+  // }
+};
